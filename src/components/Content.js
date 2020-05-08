99f08327c17b0afebfdedef2630a53d18f790349
Kumar Yogesh (3) <yogesh.kumar.3@volvo.com>
2020-05-09 00:58:05 +0200

    fixed compile issue

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 import { withStyles } from "@material-ui/core/styles";
 
